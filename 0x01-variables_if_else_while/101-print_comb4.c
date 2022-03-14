0a19241d0d35eb5ef23fcd9d91bb83e2c38512d9
felixobianozie <felixobianozie@gmail.com>
2022-03-14 11:07:33 -0700

    Replaced the file 101-print_comb4.c with an alternative code

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,41 +1,55 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Description: Print all possible different combinations of 3 digits.
- * Numbers must be separated by commas and a space.
- * The 3 digits must be different.
- * 012, 120, 102, 021, 201, 210 are considered the same combination.
- * print only the smallest combination of 3 digits.
- * Numbers should be printed in ascending order.
- * You can only use `putchar` to print to console.
- * You can only use `putchar` up to 6 times.
- * You are not allowed to use any variables of type `char`.
- * Return: 0
- */
+* main - main block
+* Description: Print all possible different combinations of 3 digits.
+* Numbers must be separated by commas and a space.
+* The 3 digits must be different.
+* 012, 120, 102, 021, 201, 210 are considered the same combination.
+* print only the smallest combination of 3 digits.
+* Numbers should be printed in ascending order.
+* You can only use `putchar` to print to console.
+* You can only use `putchar` up to 6 times.
+* You are not allowed to use any variables of type `char`.
+* Below is the Felix Obianozie way of solving it.
+* Return: 0
+*/
+
 int main(void)
 {
-	int i, j, k, l;
+	int a, c, e;
+	int b = '0';
+	int d = '0';
+	int f = '0';
 
-	for (i = 0; i < 1000; i++)
+	for (a = 0; a <= 9; a++)
 	{
-		j = i / 100; /* hundreds */
-		k = (i / 10) % 10; /* tens */
-		l = i % 10; /* singles */
-
-		if (j < k && k < l)
+		d = '0';
+		for (c = 0; c <= 9; c++)
 		{
-			putchar(j + '0');
-			putchar(k + '0');
-			putchar(l + '0');
-
-			if (i < 700)
+			f = '0';
+			for (e = 0; e <= 9; e++)
 			{
-				putchar(44);
-				putchar(32);
+				if (f > d && d > b)
+				{
+					putchar(b);
+					putchar(d);
+					putchar(f);
+					if (b == '7' && d == '8' && f == '9')
+					{
+						putchar('\n');
+					} else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				f++;
 			}
+			d++;
 		}
+		b++;
 	}
-	putchar('\n');
-
 	return (0);
 }
+
