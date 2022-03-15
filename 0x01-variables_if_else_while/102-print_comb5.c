3369cc8b65b9f1a526d0f47e047cdc5556635755
felixobianozie <felixobianozie@gmail.com>
2022-03-15 07:54:30 -0700

    Replaced 102-print_comb5.c with an alternative code

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,50 +1,83 @@
 #include <stdio.h>
+void my_print(int a, int h, int b, int d);
+
 /**
- * main - main block
- * Description: Print all possible combinations of two two-digit numbers.
- * Numbers should range from 0 to 99.
- * The two numbers should be separated by a space.
- * All numbers should be printed with two digits. 1 should be printed as 01.
- * Combination of numbers must be separated by a comma followed by a space.
- * Combinations of numbers should be printed in ascending order.
- * `00 01` and `01 00` are considered as the same combination.
- * You can only use `putchar` to print to console.
- * You can only use `putchar` up to 8 times.
- * You are not allowed to use any variable of type `char`.
- * Return: 0
- */
+* main - main function
+*
+* Description: Print all possible combinations of two two-digit numbers.
+* Numbers should range from 0 to 99.
+* The two numbers should be separated by a space.
+* All numbers should be printed with two digits. 1 should be printed as 01.
+* Combination of numbers must be separated by a comma followed by a space.
+* Combinations of numbers should be printed in ascending order.
+* `00 01` and `01 00` are considered as the same combination.
+* You can only use `putchar` to print to console.
+* You can only use `putchar` up to 8 times.  You are not allowed to use any
+* variable of type `char`.
+* We did it here in the Felix Obianozie way.
+* Section 1: program entry point.
+* Return: 0
+*/
+
 int main(void)
 {
-	int i, j;
-	int a, b, c, d;
+	int a, c, e, g;
+	int b = '0';
+	int d = '0';
+	int f = '0';
+	int h = '0';
 
-	for (i = 0; i < 100; i++)
+	for (a = 0; a <= 9; a++)
 	{
-		a = i / 10; /* doubles fnum */
-		b = i % 10; /* singles fnum */
-
-		for (j = 0; j < 100; j++)
+		d = '0';
+		for (c = 0; c <= 9; c++)
 		{
-			c = j / 10; /* doubles snum */
-			d = j % 10; /* singles snum */
-
-			if (a < c || (a == c && b < d))
+			f = '0';
+			for (e = 0; e <= 9; e++)
 			{
-				putchar(a + '0');
-				putchar(b + '0');
-				putchar(32);
-				putchar(c + '0');
-				putchar(d + '0');
-
-				if (!(a == 9 && b == 8))
+				h = '0';
+				for (g = 0; g <= 9; g++)
 				{
-					putchar(44);
-					putchar(32);
+					my_print(f, h, b, d);
+					h++;
 				}
+				f++;
+			}
+			d++;
+		}
+		b++;
+	}
+	return (0);
+}
+
+/**
+* my_print - prints to stdout
+* @f: defines third character
+* @h: defines fourth character
+* @b: defines first character
+* @d: defines second character
+*
+* Description: renders output to stdout based on certain conditions
+* Section 2: renders service to the main function
+*/
+
+void my_print(int f, int h, int b, int d)
+	{
+		if ((f + h) > (b + d))
+		{
+			putchar(b);
+			putchar(d);
+			putchar(' ');
+			putchar(f);
+			putchar(h);
+			if (b == '9' && d == '8' && f == '9' && h == '9')
+			{
+				putchar('\n');
+			} else
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
-	putchar(10);
 
-	return (0);
-}
