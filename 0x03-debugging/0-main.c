e316eb9600a45abca56cc7cd7da40e3a7ca9d3cd
felixobianozie <felixobianozie@gmail.com>
2022-03-17 23:44:31 +0100

    Fixed bugs in 0x03-debugging/0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,16 @@
 #include "main.h"
-
+void positive_or_negative(int i);
 /**
-* main - check the code
-*
-* Return: Always 0.
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
 
 int main(void)
 {
-	print_alphabet();
-	return (0);
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
 }
