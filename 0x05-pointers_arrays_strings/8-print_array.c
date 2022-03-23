22b24b45e1fdd2aa8974c7172f4d403c19359555
felixobianozie <felixobianozie@gmail.com>
2022-03-23 02:18:11 +0100

    Betty

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,12 +15,15 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i < (n-1))
+		if (i < (n - 1))
 		{
-			printf("%d, ", *(a + i));
+			printf("%d", *(a + i));
+			putchar(',');
+			putchar(' ');
 		} else
 		{
-			printf("%d\n", *(a + i));
+			printf("%d", *(a + i));
+			putchar('\n');
 		}
 	}
 }
