4f2c78eeacae8e5c959f014fa344c0619d61de78
felixobianozie <felixobianozie@gmail.com>
2022-07-01 02:09:36 +0000

    3rd modification: 100-realloc.c _ betty lint

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,45 +10,32 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *new_ptr;
-	char *old_ptr;
+	char *new_ptr, char *old_ptr;
 	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
-
 	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
 	if (ptr == NULL)
 		return (malloc(new_size));
-
-	/*Reserve new memory using malloc*/
-	new_ptr = malloc(new_size);
-
-	/*Verify that malloc did not fail*/
-	if (new_ptr == NULL)
+	new_ptr = malloc(new_size); /*Reserve new memory using malloc*/
+	if (new_ptr == NULL) /*Verify that malloc did not fail*/
 		return (NULL);
-
-	/*Cast malloc pointer to char type by reassignment*/
-	old_ptr = ptr;
-
-	/*Transfer all bytes from previous buffer to new buffer*/
-	if (new_size < old_size)
+	old_ptr = ptr; /*Cast malloc pointer to char type by reassignment*/
+	if (new_size < old_size) /*Bytes from previous buffer to new buffer*/
 	{
 		for (i = 0; i < new_size; i++)
 			new_ptr[i] = old_ptr[i];
 	}
-
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
 			new_ptr[i] = old_ptr[i];
 	}
-
 	free(ptr);
 	return (new_ptr);
 }
