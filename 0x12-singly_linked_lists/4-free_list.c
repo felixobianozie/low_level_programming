8d1e0dee3f5d8fd2844f61ac5171e2f02e840a96
felixobianozie <felixobianozie@gmail.com>
2022-07-16 20:07:56 +0100

    1st modification 4-free_list.c_earmarked pending edits

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,6 +4,7 @@
  * free_list - free the memory allocated to a linked list
  * @head: a double pointer to a linked list
  * Return: nothing
+ * THERE IS MORE WORK TO BE DONE IN THIS FUNCTION
  */
 void free_list(list_t *head)
 {
