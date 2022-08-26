af05f03de721eb77a49a72eba36d7906a839dfde
felixobianozie <felixobianozie@gmail.com>
2022-08-26 02:05:12 +0000

    Removed wrong commit

--- 0x02-python-import_modules/103-fast_alphabet.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-
-if __name__ == "__main__":    
-    for i in range(65, 91):
-        print("{}".format(chr(i)), end="")
-    print()
--- 0x02-python-import_modules/README.md
+++
@@ -1,1 +0,0 @@
-Python Import and module projects
